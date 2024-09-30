9332f4b071ddc678e5832ca59673b6fd3f4c40ff
daahyunk <ekgusdl0408@naver.com>
2024-09-30 19:27:57 +0900

    📝 더미데이터 수정

--- src/utils/nearbyTourData.js
+++ src/utils/nearbyTourData.js
@@ -6,7 +6,7 @@ const nearbyTourData = [
     },
     {
       image: '/src/assets/images/changdeokgung.png',
-      title: '창경궁 : 도심 속 궁나들이',
+      title: '경복궁 : 배부르다',
       subtitle: '서울 종로구 창경궁로 185',
     },
   ];
