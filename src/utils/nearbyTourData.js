dcc8d2082f82b9a54f42a99bca3c4e8e7af04ee1
daahyunk <ekgusdl0408@naver.com>
2024-10-02 01:12:48 +0900

    🔧 NearbyTourData 파일 수정

--- src/utils/nearbyTourData.js
+++ src/utils/nearbyTourData.js
@@ -1,15 +1,58 @@
 const nearbyTourData = [
-    {
-      image: '/src/assets/images/changdeokgung.png',
-      title: '창경궁 : 도심 속 궁나들이',
-      subtitle: '서울 종로구 창경궁로 185',
+  {
+    image: '/src/assets/images/changdeokgung.png',
+    image2: '/src/assets/images/chang.png',
+    title: '창경궁1',
+    subtitle: '서울 종로구 창경궁로 185',
+    accessibility: {
+      wheelchair: true,
+      family: true,
+      stroller: true,
+      visual: false,
+      hearing: true,
     },
-    {
-      image: '/src/assets/images/changdeokgung.png',
-      title: '경복궁 : 배부르다',
-      subtitle: '서울 종로구 창경궁로 185',
+  },
+  {
+    image: '/src/assets/images/changdeokgung.png',
+    image2: '/src/assets/images/chang.png',
+    title: '경복궁2',
+    subtitle: '서울 종로구 창경궁로 185',
+    accessibility: {
+      wheelchair: true,
+      family: true,
+      stroller: true,
+      visual: false,
+      hearing: true,
     },
-  ];
+  },
+  {
+    image: '/src/assets/images/changdeokgung.png',
+    image2: '/src/assets/images/chang.png',
+    title: '경복궁3',
+    subtitle: '서울 종로구 창경궁로 185',
+    accessibility: {
+      wheelchair: true,
+      family: true,
+      stroller: true,
+      visual: false,
+      hearing: true,
+    },
+  },
+  {
+    image: '/src/assets/images/changdeokgung.png',
+    image2: '/src/assets/images/chang.png',
+    title: '경복궁4',
+    subtitle: '서울 종로구 창경궁로 185',
+    accessibility: {
+      wheelchair: true,
+      family: true,
+      stroller: true,
+      visual: false,
+      hearing: true,
+    },
+  },
   
-  export default nearbyTourData;
-  
+  // 추가적인 데이터...
+];
+
+export default nearbyTourData;
