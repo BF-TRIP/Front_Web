4b485b3c63b5cf79ee4bc6a853c285221e1af7e9
daahyunk <ekgusdl0408@naver.com>
2024-10-03 16:03:27 +0900

    ✨ TitleSection 컴포넌트 추가

--- src/pages/home/detail/Detail.jsx
+++ src/pages/home/detail/Detail.jsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import DetailTop from '../../../components/home/detail/DetailTop'; 
+import TitleSection from '../../../components/home/detail/TitleSection'; 
 
 const DetailPageContainer = styled.div`
   width: 390px;
@@ -8,6 +9,7 @@ const DetailPageContainer = styled.div`
   overflow-y: auto;
 `;
 
+
 const Detail = () => {
   const images = [
     '/path/to/image1.jpg',
@@ -15,9 +17,19 @@ const Detail = () => {
     '/path/to/image3.jpg',
   ]; 
 
+  // 임시 데이터
+  const placeName = '덕수궁';
+  const description = '덕수궁(德壽宮)은 서울 중구 정동(貞洞)에 있는 조선과 대한제국의 궁궐이다. 대한민국의 사적으로 지정되어 있다. 본래 이름은 경운궁(慶運宮)으로, 아관파천 이후 환궁하여 법궁으로 사용되다가 순종 즉위 후 궁의 이름을 현재의 덕수궁으로 변경하였다.';
+  const address = '서울특별시 중구 세종대로 99';
+
   return (
     <DetailPageContainer>
       <DetailTop images={images} />
+      <TitleSection 
+        placeName={placeName}
+        description={description}
+        address={address}
+      />
     </DetailPageContainer>
   );
 };
