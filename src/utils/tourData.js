32b54bbdd59efc68e74162e3690bca7dc59bbecb
daahyunk <ekgusdl0408@naver.com>
2024-10-04 18:51:33 +0900

    ✨ TourData 추가

---
+++ src/utils/tourData.js
@@ -0,0 +1,20 @@
+const tourData = [
+    {
+      id: 1,
+      image: '/path/to/image1.jpg',
+      title: '부산 2박 3일 코스',
+    },
+    {
+        id: 2,
+        image: '/path/to/image1.jpg',
+        title: '부산 2박 3일 코스',
+      },
+      {
+        id: 3,
+        image: '/path/to/image1.jpg',
+        title: '부산 2박 3일 코스',
+      },
+  ];
+  
+  export default tourData;
+  
