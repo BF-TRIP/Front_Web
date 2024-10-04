cdf7cf7d3a0a5b8a77bfe8682c2687ded12b3b79
daahyunk <ekgusdl0408@naver.com>
2024-10-04 20:09:04 +0900

    ✨ 데이터 수정

---
+++ src/utils/savedTourData.js
@@ -0,0 +1,21 @@
+const savedTourData = [
+    {
+      image: '/src/assets/images/chang2.png',
+      title: '창경궁',
+    },
+    {
+      image: '/path/to/image2.jpg',
+      title: '한라산 국립공원2222',
+    },
+    {
+      image: '/path/to/image3.jpg',
+      title: '경복궁',
+    },
+    {
+      image: '/path/to/image4.jpg',
+      title: '창덕궁',
+    },
+  ];
+  
+  export default savedTourData;
+  
--- src/utils/tourData.js
+++ src/utils/tourData.js
@@ -4,16 +4,6 @@ const tourData = [
       image: '/path/to/image1.jpg',
       title: '부산 2박 3일 코스',
     },
-    {
-        id: 2,
-        image: '/path/to/image1.jpg',
-        title: '부산 2박 3일 코스',
-      },
-      {
-        id: 3,
-        image: '/path/to/image1.jpg',
-        title: '부산 2박 3일 코스',
-      },
   ];
   
   export default tourData;
