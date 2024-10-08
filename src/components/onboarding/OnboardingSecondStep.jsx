19cb3e8c0f62ce57ebe34421f638c1b5e3437f8b
daahyunk <ekgusdl0408@naver.com>
2024-10-09 01:41:26 +0900

    ✨ 온보딩 컴포넌트에 context 추가

--- src/components/onboarding/OnboardingSecondStep.jsx
+++ src/components/onboarding/OnboardingSecondStep.jsx
@@ -1,11 +1,12 @@
 import styled from 'styled-components';
 import PropTypes from 'prop-types'; 
-import { useEffect, useState } from 'react'; 
+import { useEffect, useState, useContext } from 'react'; 
 import { useNavigate } from 'react-router-dom'; 
 import Header from './Header'; 
 import NextButton from './NextButton'; 
 import Title from './onboarding2/Title'; 
 import HandicapSelection from './onboarding2/HandicapSelection'; 
+import { OnboardingContext } from '../../utils/OnboardingContext'; 
 
 const Container = styled.div`
   width: 100%;
@@ -18,6 +19,8 @@ const InfoContainer = styled.div`
 `;
 
 const OnboardingSecondStep = ({ progress }) => {
+  const { onboardingData, updateOnboardingData } = useContext(OnboardingContext);
+  const [selectedHandicaps, setSelectedHandicaps] = useState(onboardingData.selectedHandicaps || {}); // 기존 선택값 불러오기
   const [localProgress, setLocalProgress] = useState(progress); 
   const navigate = useNavigate(); 
 
@@ -31,7 +34,10 @@ const OnboardingSecondStep = ({ progress }) => {
   }, []);
 
   const handleNext = () => {
-    setLocalProgress(80);
+    // 현재 상태를 Context에 저장
+    updateOnboardingData('selectedHandicaps', selectedHandicaps);
+
+    setLocalProgress(75);
     setTimeout(() => {
       navigate('/onboarding-third'); 
     }, 500);
@@ -41,12 +47,16 @@ const OnboardingSecondStep = ({ progress }) => {
     navigate(-1); 
   };
 
+  const handleHandicapSelectionChange = (newSelections) => {
+    setSelectedHandicaps(newSelections); 
+  };
+
   return (
     <Container>
       <Header onBack={handleBack} progress={localProgress} showBackButton={true} />
       <InfoContainer>
         <Title /> 
-        <HandicapSelection />
+        <HandicapSelection onSelectionChange={handleHandicapSelectionChange} selectedHandicaps={selectedHandicaps} />
       </InfoContainer>
       <NextButton onClick={handleNext} /> 
     </Container>
