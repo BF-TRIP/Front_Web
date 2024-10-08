ee1df55e6854750f9adebf5bce7e1a79c91a916a
daahyunk <ekgusdl0408@naver.com>
2024-10-08 16:20:56 +0900

    ✨ 통합 컴포넌트에 생년월일 및 다음 컴포넌트 추가

--- src/components/onboarding/OnboardingFirstStep.jsx
+++ src/components/onboarding/OnboardingFirstStep.jsx
@@ -4,21 +4,29 @@ import { useState } from 'react';
 import Header from './Header'; 
 import NameInputSection from './onboarding1/NameInputSection'; 
 import GenderSelectionSection from './onboarding1/GenderSelectionSection';
+import BirthdaySection from './onboarding1/BirthdaySection'; 
+import NextButton from './NextButton'; 
 
 const Container = styled.div`
   width: 100%;
   height: 100%;
   background-color: #fff;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
 `;
 
 const InfoContainer = styled.div`
-  padding: 1rem;
+  padding: 0.8rem;
+  flex-grow: 1; 
 `;
 
-
-const OnboardingFirstStep = ({ onBack, progress }) => {
+const OnboardingFirstStep = ({ onBack, progress, onNext }) => {
   const [name, setName] = useState(''); // 이름 입력 상태 관리
   const [gender, setGender] = useState(''); // 성별 선택 상태 관리
+  const [year, setYear] = useState(''); // 생년 상태 관리
+  const [month, setMonth] = useState(''); // 월 상태 관리
+  const [day, setDay] = useState(''); // 일 상태 관리
 
   const handleNameChange = (e) => {
     setName(e.target.value);
@@ -28,13 +36,34 @@ const OnboardingFirstStep = ({ onBack, progress }) => {
     setGender(selectedGender); // 성별 선택 상태 업데이트
   };
 
+  const handleYearChange = (e) => {
+    setYear(e.target.value); // 생년 상태 업데이트
+  };
+
+  const handleMonthChange = (e) => {
+    setMonth(e.target.value); // 월 상태 업데이트
+  };
+
+  const handleDayChange = (e) => {
+    setDay(e.target.value); // 일 상태 업데이트
+  };
+
   return (
     <Container>
-    <Header onBack={onBack} progress={progress} />
-        <InfoContainer> 
-            <NameInputSection value={name} onChange={handleNameChange} />
-            <GenderSelectionSection selectedGender={gender} onGenderSelect={handleGenderSelect} />
-        </InfoContainer>
+      <Header onBack={onBack} progress={progress} />
+      <InfoContainer> 
+        <NameInputSection value={name} onChange={handleNameChange} />
+        <GenderSelectionSection selectedGender={gender} onGenderSelect={handleGenderSelect} />
+        <BirthdaySection 
+          year={year} 
+          month={month} 
+          day={day} 
+          onYearChange={handleYearChange} 
+          onMonthChange={handleMonthChange} 
+          onDayChange={handleDayChange} 
+        />
+      </InfoContainer>
+      <NextButton onClick={onNext} /> 
     </Container>
   );
 };
@@ -42,6 +71,7 @@ const OnboardingFirstStep = ({ onBack, progress }) => {
 OnboardingFirstStep.propTypes = {
   onBack: PropTypes.func.isRequired, 
   progress: PropTypes.number.isRequired, 
+  onNext: PropTypes.func.isRequired, 
 };
 
 export default OnboardingFirstStep;
