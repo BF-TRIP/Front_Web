8a83c7042169c2fbcdf5bfdc057033f653a7eae4
daahyunk <ekgusdl0408@naver.com>
2024-10-08 21:27:45 +0900

    ✨ 온보딩2 페이지 컴포넌트

---
+++ src/pages/onboarding/OnboardingSecondPage.jsx
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+import PropTypes from 'prop-types'; 
+import OnboardingSecondStep from '../../components/onboarding/OnboardingSecondStep'; 
+
+const PageContainer = styled.div`
+  width: 390px;
+  height: 844px;
+  background-color: #fff;
+`;
+
+const OnboardingSecondPage = ({ onBack }) => {
+  return (
+    <PageContainer>
+      <OnboardingSecondStep onBack={onBack} progress={50} /> 
+    </PageContainer>
+  );
+};
+
+OnboardingSecondPage.propTypes = {
+  onBack: PropTypes.func.isRequired, 
+};
+
+export default OnboardingSecondPage;
