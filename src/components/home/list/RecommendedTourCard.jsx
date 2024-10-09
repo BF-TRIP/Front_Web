a081f30aca381a8c64d5eaa4434f200606f33ed4
daahyunk <ekgusdl0408@naver.com>
2024-10-10 07:12:04 +0900

    ✨ 유저 특성 관광지 추천 리스트 api 연결 및 ui 수정

--- src/components/home/list/RecommendedTourCard.jsx
+++ src/components/home/list/RecommendedTourCard.jsx
@@ -1,146 +1,131 @@
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
-import WheelchairIcon from '../../../assets/images/wheelchair.svg';
-import FamilyIcon from '../../../assets/images/family.svg';
-import StrollerIcon from '../../../assets/images/stroller.svg';
-import VisualIcon from '../../../assets/images/visual.svg';
-import HearingIcon from '../../../assets/images/hearing.svg';
+import WheelchairIcon from '../../../assets/images/senior.png';
+import FamilyIcon from '../../../assets/images/wheelchair.png';
+import StrollerIcon from '../../../assets/images/infant.png';
+import VisualIcon from '../../../assets/images/blind.png';
+import HearingIcon from '../../../assets/images/deaf.png';
 
 const CardContainer = styled.div`
-  width: 22.375rem;
-  height: auto;
-  display: flex;
-  flex-direction: column;
-  flex-shrink: 0;
+  width: 100%; 
+  background-color: #fff;
+  border-radius: 1rem;
+  box-shadow: 0px 4px 20px 0px rgba(0, 0, 0, 0.08);
+  margin-bottom: 1rem;
   overflow: hidden;
-  border-radius: 1.125rem;
-  padding: 0.62rem;
-  background: #FFF;
-  box-shadow: 0px 0px 20px 0px rgba(0, 0, 0, 0.08);
 `;
 
-const CardImage = styled.img`
-  width: 21.125rem;
-  height: 9.8125rem;
-  border-radius: 0.75rem;
-  flex-shrink: 0;
+const CardImage = styled.div`
+  width: 100%;
+  height: 12rem;
+  border-top-left-radius: 1rem;
+  border-top-right-radius: 1rem;
+  background-image: ${(props) =>
+    props.imageUrl ? `url(${props.imageUrl})` : 'none'};
+  background-color: #ddd;
+  background-size: cover;
+  background-position: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const PlaceholderIcon = styled.img`
+  width: 3rem;
+  opacity: 0.5;
+  display: ${(props) => (props.imageUrl ? 'none' : 'block')};  
 `;
 
 const CardTitleRow = styled.div`
+  padding: 1rem;
   display: flex;
-  align-items: center; 
-  padding: 0 0.5rem;
-  margin-bottom: 0.5rem;
+  align-items: center;
 `;
 
-const CardTitle = styled.div`
-  color: #000;
-  text-align: center;
-  font-size: 1.375rem;
-  font-style: normal;
+const CardTitle = styled.h2`
+  font-size: 1.125rem;
   font-weight: 600;
-  line-height: 150%;
+  color: #000;
 `;
 
-const CardSubtitle = styled.div`
-  color: #000;
-  text-align: center;
-  font-size: 0.75rem;
-  font-style: normal;
-  font-weight: 400;
-  line-height: 150%;
-  margin-left: 0.5rem;
+const CardSubtitle = styled.p`
+  font-size: 0.875rem;
+  color: #7a7a7a;
+  margin-left: 0.53rem;
 `;
 
 const AccessibilityIcons = styled.div`
   display: flex;
-  justify-content: space-evenly; 
-  align-items: center;
-  margin-top: 0.75rem;
-  gap: 1.4rem;
+  justify-content: space-around;
+  padding-bottom: 1rem;
 `;
 
-const WheelchairIconStyled = styled.img`
-  width: 1.75rem;
-  height: 2.125rem;
-  flex-shrink: 0;
-`;
-
-const FamilyIconStyled = styled.img`
-  width: 2.125rem;
-  height: 2.00719rem;
-  flex-shrink: 0;
-`;
-
-const StrollerIconStyled = styled.img`
-  width: 2.125rem;
-  height: 1.94506rem;
-  flex-shrink: 0;
-`;
-
-const VisualIconStyled = styled.img`
-  width: 2.75rem;
-  height: 2.75rem;
-  flex-shrink: 0;
-`;
-
-const HearingIconStyled = styled.img`
-  width: 1.625rem;
-  height: 2.375rem;
-  flex-shrink: 0;
+const Icon = styled.img`
+  width: auto;
+  height: 2.5rem;
+  opacity: ${(props) => (props.isActive ? '1' : '0.3')};
 `;
 
 const RecommendedTourCard = ({
-  image,
+  imageUrl,
   title,
-  subtitle,
-  accessibility = {
-    wheelchair: false,
-    family: false,
-    stroller: false,
-    visual: false,
-    hearing: false,
-  }
+  address,
+  publicTransport,
+  wheelchair,
+  stroller,
+  braileBlock,
+  hearingHandicapEtc,
 }) => {
   return (
     <CardContainer>
+      <CardImage imageUrl={imageUrl}>
+        <PlaceholderIcon src="src/assets/images/image.png" alt="Placeholder Icon" imageUrl={imageUrl} />
+      </CardImage>
       <CardTitleRow>
         <CardTitle>{title}</CardTitle>
-        <CardSubtitle>{subtitle}</CardSubtitle>
+        <CardSubtitle>{address}</CardSubtitle>
       </CardTitleRow>
-      <CardImage src={image} alt={title} />
       <AccessibilityIcons>
-        {accessibility.wheelchair && (
-          <WheelchairIconStyled src={WheelchairIcon} alt="Wheelchair accessible" />
-        )}
-        {accessibility.family && (
-          <FamilyIconStyled src={FamilyIcon} alt="Family friendly" />
-        )}
-        {accessibility.stroller && (
-          <StrollerIconStyled src={StrollerIcon} alt="Stroller accessible" />
-        )}
-        {accessibility.visual && (
-          <VisualIconStyled src={VisualIcon} alt="Visual aid available" />
-        )}
-        {accessibility.hearing && (
-          <HearingIconStyled src={HearingIcon} alt="Hearing aid available" />
-        )}
+        <Icon 
+          src={FamilyIcon} 
+          alt="Family Friendly" 
+          isActive={!!publicTransport} 
+        />
+        <Icon 
+          src={WheelchairIcon} 
+          alt="Wheelchair Accessible" 
+          isActive={!!wheelchair} 
+        />
+        <Icon 
+          src={VisualIcon} 
+          alt="Braille Block" 
+          isActive={!!braileBlock} 
+        />
+        <Icon 
+          src={StrollerIcon} 
+          alt="Stroller Accessible" 
+          isActive={!!stroller} 
+        />
+
+        <Icon 
+          src={HearingIcon} 
+          alt="Hearing Aid Accessible" 
+          isActive={!!hearingHandicapEtc} 
+        />
       </AccessibilityIcons>
     </CardContainer>
   );
 };
 
 RecommendedTourCard.propTypes = {
-  image: PropTypes.string.isRequired,
+  imageUrl: PropTypes.string,
   title: PropTypes.string.isRequired,
-  subtitle: PropTypes.string.isRequired,
-  accessibility: PropTypes.shape({
-    wheelchair: PropTypes.bool,
-    family: PropTypes.bool,
-    stroller: PropTypes.bool,
-    visual: PropTypes.bool,
-    hearing: PropTypes.bool,
-  }),
+  address: PropTypes.string.isRequired,
+  publicTransport: PropTypes.string,
+  wheelchair: PropTypes.string,
+  stroller: PropTypes.string,
+  braileBlock: PropTypes.string,
+  hearingHandicapEtc: PropTypes.string,
 };
 
 export default RecommendedTourCard;
