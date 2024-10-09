33ecdc4451579e13c560466f24bc64eb41255c08
daahyunk <ekgusdl0408@naver.com>
2024-10-10 00:42:48 +0900

    👽️ 저장한 관광지 api 연결

--- src/pages/savelist/Savelist.jsx
+++ src/pages/savelist/Savelist.jsx
@@ -1,4 +1,6 @@
-import { useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
+import { OnboardingContext } from '../../utils/OnboardingContext';
+import getSavedTours from '../../api/save/getSavedTours';
 import styled from 'styled-components';
 import Header from '../../components/common/Header';
 import MyTourList from '../../components/savelist/MyTourList';
@@ -6,23 +8,32 @@ import SavedTourList from '../../components/savelist/SavedTourList';
 import NewListModal from '../../components/savelist/newlist/NewListModal';
 
 const SaveListContainer = styled.div`
-  position: relative; /* 모달이 이 부모 컴포넌트 안에서만 위치하게 설정 */
+  position: relative;
   width: 390px;
   height: 844px;
   background-color: #fff;
   overflow-y: auto;
   padding-bottom: 10em;
-
-  &::-webkit-scrollbar {
-    display: none;
-  }
-  
-  -ms-overflow-style: none;
-  scrollbar-width: none;
 `;
 
 const Savelist = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [savedTourList, setSavedTourList] = useState([]);
+  
+  const { onboardingData, updateOnboardingData } = useContext(OnboardingContext);
+  let { userNumber } = onboardingData;
+
+  useEffect(() => {
+    if (!userNumber) {
+      const savedUserNumber = localStorage.getItem('userNumber');
+      if (savedUserNumber) {
+        userNumber = savedUserNumber;
+        updateOnboardingData('userNumber', savedUserNumber); 
+      } else {
+        console.warn('userNumber가 존재하지 않습니다. 홈으로 리디렉션 필요.');
+      }
+    }
+  }, [userNumber, updateOnboardingData]);
 
   const handleOpenModal = () => {
     setIsModalOpen(true);
@@ -37,12 +48,34 @@ const Savelist = () => {
     setIsModalOpen(false);
   };
 
+  useEffect(() => {
+    if (userNumber) {
+      console.log('userNumber:', userNumber); 
+      fetchSavedTourList(userNumber); // Context 또는 localStorage에서 받아온 userNumber 사용
+    }
+  }, [userNumber]);
+
+  const fetchSavedTourList = async (userNumber) => {
+    try {
+      const data = await getSavedTours(userNumber);
+      
+      // 받아온 데이터를 변환해서 저장
+      const transformedData = data.map((tour) => ({
+        contentTitle: tour.content_title, // 백엔드의 필드명을 프론트엔드에 맞게 변환
+        thumbnailImage: tour.thumbnail_image, // 백엔드의 필드명을 프론트엔드에 맞게 변환
+      }));
+
+      setSavedTourList(transformedData); // 변환된 데이터를 상태에 저장
+    } catch (error) {
+      console.error('저장한 관광지 목록 불러오기 실패:', error);
+    }
+  };
+
   return (
     <SaveListContainer>
       <Header />
       <MyTourList onOpenModal={handleOpenModal} />
-      <SavedTourList />
-      {/* 모달 추가 */}
+      <SavedTourList savedTourData={savedTourList} />
       <NewListModal show={isModalOpen} onClose={handleCloseModal} onConfirm={handleConfirm} />
     </SaveListContainer>
   );
