a29f71008e86ed74c3742e4c70298bfc55b8cc3b
daahyunk <ekgusdl0408@naver.com>
2024-10-10 03:47:07 +0900

    ✨ 리스트 네임 전달

--- src/components/savelist/AddToList.jsx
+++ src/components/savelist/AddToList.jsx
@@ -2,6 +2,9 @@ import styled from 'styled-components';
 import AddToListHeader from './addlist/AddToListHeader'; 
 import SavedTourList from '../savelist/SavedTourList'; 
 import AddToListFooter from './addlist/AddToListFooter';
+import { useEffect, useState } from 'react';
+import getSavedTours from '../../api/save/getSavedTours'; 
+import PropTypes from 'prop-types'; 
 
 const Container = styled.div`
   width: 100%;
@@ -17,14 +20,42 @@ const Container = styled.div`
   scrollbar-width: none;
 `;
 
-const AddToList = () => {
+const AddToList = ({ userNumber, listName, onBack }) => {
+  const [savedTourData, setSavedTourData] = useState([]);
+
+  const fetchSavedTourData = async () => {
+    try {
+      const data = await getSavedTours(userNumber);
+      const transformedData = data.map(tour => ({
+        ...tour,
+        thumbnailImage: tour.thumbnail_image, 
+        contentTitle: tour.content_title
+      }));
+      setSavedTourData(transformedData);
+    } catch (error) {
+      console.error('저장한 관광지 데이터를 불러오는 중 에러가 발생했습니다:', error);
+    }
+  };
+
+  useEffect(() => {
+    if (userNumber) {
+      fetchSavedTourData();
+    }
+  }, [userNumber]);
+
   return (
     <Container>
-      <AddToListHeader /> 
-      <SavedTourList showSubtitle={true} showCheckbox={true} /> 
+      <AddToListHeader listName={listName} onBack={onBack} />  
+      <SavedTourList showCheckbox={true} savedTourData={savedTourData} />
       <AddToListFooter /> 
     </Container>
   );
+};
+
+AddToList.propTypes = {
+  userNumber: PropTypes.string.isRequired, 
+  listName: PropTypes.string.isRequired, 
+  onBack: PropTypes.func.isRequired, 
 };
 
 export default AddToList;
