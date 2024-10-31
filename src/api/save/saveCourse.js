2439c59f809ee7d9cc60171ef2e94b918657343b
daahyunk <ekgusdl0408@naver.com>
2024-10-31 10:49:30 +0900

    ✏️ SaveCourses body 형식 수정

--- src/api/save/saveCourse.js
+++ src/api/save/saveCourse.js
@@ -4,12 +4,18 @@ import BASE_URL from '../config';
 const saveCourse = async (uuid, contentId) => {
   try {
     const response = await axios.post(
-      `${BASE_URL}/course/save?uuid=${uuid}&contentId=${contentId}`
+      `${BASE_URL}/course/save`,  // URL에 쿼리 파라미터 대신
+      { uuid, contentId },        // JSON 형식의 요청 본문으로 포함
+      {
+        headers: {
+          'Content-Type': 'application/json'  
+        }
+      }
     );
-    return response.data; 
+    return response.data;
   } catch (error) {
     console.error('저장 요청 중 오류 발생:', error);
-    throw error; 
+    throw error;
   }
 };
 
